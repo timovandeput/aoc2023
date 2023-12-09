e62f7073f678649a88b6afb97d152284e195383d
Timo van de Put <timo.van.de.put@software101.nl>
2023-12-09 16:46:48 +0100

    feat: Solves part two of day 1
    Trick is to also allow for concatenated digits, like "twone" (=21).

--- lib/calibration.dart
+++ lib/calibration.dart
@@ -3,9 +3,50 @@ class Calibration {
 
   final String text;
 
-  int get value {
-    final notDigit = RegExp(r'\D+');
-    final digits = text.replaceAll(notDigit, '');
-    return int.parse(digits[0] + digits[digits.length - 1]);
+  int get value => 10 * text.firstDigit + text.lastDigit;
+}
+
+extension on String {
+  int get firstDigit {
+    for (var index = 0; index < length; index++) {
+      for (final key in _digits.keys) {
+        if (substring(index).startsWith(key)) {
+          return _digits[key]!;
+        }
+      }
+    }
+    throw RangeError('No digit found in $this');
+  }
+
+  int get lastDigit {
+    for (var index = length; index > 0; index--) {
+      for (final key in _digits.keys) {
+        if (substring(0, index).endsWith(key)) {
+          return _digits[key]!;
+        }
+      }
+    }
+    throw RangeError('No digit found in $this');
   }
 }
+
+const _digits = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+};
--- test/calibration_test.dart
+++ test/calibration_test.dart
@@ -7,6 +7,42 @@ void main() {
       expect(Calibration('12').value, equals(12));
       expect(Calibration('7').value, equals(77));
       expect(Calibration('a1b2c3d').value, equals(13));
+
+      expect(Calibration('1abc2').value, equals(12));
+      expect(Calibration('pqr3stu8vwx').value, equals(38));
+      expect(Calibration('a1b2c3d4e5f').value, equals(15));
+      expect(Calibration('treb7uchet').value, equals(77));
+    });
+
+    test('converts words to digits', () {
+      expect(Calibration('one').value, equals(11));
+      expect(Calibration('two').value, equals(22));
+      expect(Calibration('three').value, equals(33));
+      expect(Calibration('four').value, equals(44));
+      expect(Calibration('five').value, equals(55));
+      expect(Calibration('six').value, equals(66));
+      expect(Calibration('seven').value, equals(77));
+      expect(Calibration('eight').value, equals(88));
+      expect(Calibration('nine').value, equals(99));
+    });
+
+    test('expands embedded words to digits', () {
+      expect(Calibration('two1nine').value, equals(29));
+      expect(Calibration('eightwothree').value, equals(83));
+      expect(Calibration('abcone2threexyz').value, equals(13));
+      expect(Calibration('xtwone3four').value, equals(24));
+      expect(Calibration('4nineeightseven2').value, equals(42));
+      expect(Calibration('zoneight234').value, equals(14));
+      expect(Calibration('7pqrstsixteen').value, equals(76));
+    });
+
+    test('decodes concatenated words', () {
+      expect(Calibration('twone').value, equals(21));
+      expect(Calibration('abtwonecd').value, equals(21));
+    });
+
+    test('ignores any zeroes', () {
+      expect(Calibration('zero120').value, equals(12));
     });
 
     test('throws for no digits', () {
